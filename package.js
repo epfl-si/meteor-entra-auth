96fe85e372cc18c9b0e152b82548184d400d4557
Julien Delasoie <julien.delasoie@epfl.ch>
2025-03-13 11:39:46 +0100

    Fix the 'OAuth not declared' error by adding the oauth package

--- package.js
+++ package.js
@@ -1,15 +1,19 @@
 Package.describe({
   name: 'epfl:entra-oauth',
-  version: '0.0.1',
+  version: '0.0.3',
   summary: 'Use Microsoft Entra to login, for Meteor 2.16.',
   git: 'https://github.com/epfl-si/meteor-entra-auth',
 });
 
 Package.onUse(function(api) {
-  api.versionsFrom('2.16');
+  // despite saying it works with 3.0,
+  // it has not been tested. The value is added only to escape
+  // the dependencies versions hell between oauth and oauth2 package
+  api.versionsFrom(['2.16', '3.0']);
 
-  api.use('ecmascript');
+  api.use('oauth');
   api.use('oauth2');
+  api.use('ecmascript');
   api.use('fetch', 'server');
   api.use('random', 'client');
   api.use('service-configuration');
